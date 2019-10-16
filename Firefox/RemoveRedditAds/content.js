67949b3c289c4f06d5762342ea49e89c464ba677
Ted Williams <twilliams@omahait.com>
2019-10-16 10:30:31 -0500

    Update FF Extension

--- Firefox/RemoveRedditAds/content.js
+++ Firefox/RemoveRedditAds/content.js
@@ -1,10 +1,9 @@
 if (window.location.hostname === 'www.reddit.com') {
-    window.location.hostname = 'old.reddit.com'
+    window.location.hostname = 'old.reddit.com';
+} else if (window.location.hostname === 'old.reddit.com') {
+    var ads = document.querySelector(".promoted");
+    var gold = document.querySelector(".premium-banner");
+    ads.parentNode.removeChild(ads);
+    gold.parentNode.removeChild(gold);
+    console.log("Hidden Ads Removed");
 }
-var ads = document.querySelector(".promoted");
-var gold = document.querySelector(".premium-banner");
-
-ads.parentNode.removeChild(ads);
-gold.parentNode.removeChild(gold);
-
-console.log("Hidden Ads Removed");
