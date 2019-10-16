5db93708ed068e8a9259cf6efc86d3bb5b1b6951
Ted Williams <twilliams@omahait.com>
2019-10-16 09:59:07 -0500

    Update Chrome Extension

--- Chrome/RemoveAdsReddit/content.js
+++ Chrome/RemoveAdsReddit/content.js
@@ -1,4 +1,10 @@
-$( ".promoted" ).remove();
-$(".premium-banner").remove();
+if (window.location.hostname === 'www.reddit.com') {
+    window.location.hostname = 'old.reddit.com'
+}
+var ads = document.querySelector(".promoted");
+var gold = document.querySelector(".premium-banner");
+
+ads.parentNode.removeChild(ads);
+gold.parentNode.removeChild(gold);
 
 console.log("Hidden Ads Removed");
